4f6dd825c25b155efef0d2f2f04b0edf0fa94092
shwethachandu <shwethab.xworkz@gmail.com>
2022-09-15 08:30:47 +0530

    mobile runner code

---
+++ MobileNumber.java
@@ -0,0 +1,60 @@
+class MobileNumber{
+	
+	static String getNameByNumber(long number)
+	{
+		System.out.println("Entered get NameByNumber");
+		if(number==7019234547L)
+		{
+			System.out.println("Shwetha Number is 7019234547L");
+			return "Shwetha";
+		}
+		if(number==8654739286L)
+		{
+			System.out.println("Prameela Number is 8654739286L");
+			return "Prameela";
+		}
+		if(number==7125346869L)
+		{
+			System.out.println("Navyashree Number is 7125346869L");
+			return "Navyashree";
+		}
+		if(number==7019234548L)
+		{
+			System.out.println("Spoorthi Number is 7019234548L");
+			return "Spoorthi";
+		}
+		if(number==7019234541L)
+		{
+			System.out.println("Sanjana Number is 7019234541L");
+			return "Sanjana";
+		}
+		if(number==7019234542L)
+		{
+			System.out.println("Kavya Number is 7019234542L");
+			return "Kavya";
+		}
+		if(number==7019234543L)
+		{
+			System.out.println("Pallavi Number is 7019234543L");
+			return "Pallavi";
+		}
+		if(number==9711263457L)
+		{
+			System.out.println("Megha Number is 9711263457L");
+			return "Megha";
+		}
+		if(number==9900559966L)
+		{
+			System.out.println("Pooja Number is 9900559966L");
+			return "Pooja";
+		}
+		if(number==8310894408L)
+		{
+			System.out.println("Yeshu Number is 8310894408L");
+			return "Yeshu";
+		}
+		return null;
+		
+	}
+	
+}
