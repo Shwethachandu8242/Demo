d57ef99029ce5a31e875d4b67375d345afb2b5f6
Shwethachandu8242 <113550294+Shwethachandu8242@users.noreply.github.com>
2022-09-22 08:29:12 +0530

    Add files via upload

---
+++ ToothPasteRunner.java
@@ -0,0 +1,64 @@
+class ToothPasteRunner
+{
+	public static void main(String[] brand)
+	{
+		ToothPaste.setcompanyName("closeUp");                  
+		ToothPaste.setProductHighlights("Neam,Triphale");
+		ToothPaste.setModelName("VajradantiInDalchiniFlavour");
+		ToothPaste.setIdeal_For("MenAndWomen");
+		ToothPaste.setMinimumAge("24month");
+		ToothPaste.setMaximumAge("36months");
+		ToothPaste.setAppliedFor("germsAndTeeth");
+		ToothPaste.setVegetarian(true);
+		ToothPaste.setOrganicType("ayurvedic");
+		ToothPaste.setIngredient("ayurvedic");
+		ToothPaste.setFlovor("cinnamon");
+		ToothPaste.setprice(284);
+		ToothPaste.setquantity(480);
+		ToothPaste.setavailable(true);
+		ToothPaste.setratings(4.5d);
+		ToothPaste.setShape("cylindrical");
+		ToothPaste.sethelpProtect(true);
+		ToothPaste.setSuitable("adults");
+		ToothPaste.setPack(4);
+		ToothPaste.setVariants("Tulsi,Neem,Clove");
+		ToothPaste.setcolor("white");
+		
+		String companyName=ToothPaste.getcompanyName();
+		String ProductHighlights=ToothPaste.getProductHighlights();
+		String ModelName=ToothPaste.getModelName();
+		String Ideal_For=ToothPaste.getIdeal_For();
+		String MinimumAge=ToothPaste.getMinimumAge();
+		String MaximumAge=ToothPaste.getMaximumAge();
+		String AppliedFor=ToothPaste.getAppliedFor();
+		boolean Vegetarian=ToothPaste.getVegetarian();
+		String OrganicType=ToothPaste.getOrganicType();
+		String Ingredient=ToothPaste.getIngredient();
+		String Flovor=ToothPaste.getFlovor();
+		double price1=ToothPaste.getprice1();
+		int quantity=ToothPaste.getquantity();
+		boolean available=ToothPaste.getavailable();
+		double ratings=ToothPaste.getratings();
+		String Shape=ToothPaste.getShape();
+		boolean helpProtect=ToothPaste.gethelpProtect();
+		String Suitable=ToothPaste.getSuitable();
+		int Pack=ToothPaste.getPack();
+		String Variants=ToothPaste.getVariants();
+		String color=ToothPaste.getcolor();
+		
+		if(companyName="closeUp"  &&  ProductHighlights="Neam,Triphale" && ModelName="VajradantiInDalchiniFlavour" && Ideal_For="MenAndWomen" && MinimumAge="24month" &&  MaximumAge="36months" && AppliedFor="germsAndTeeth"  &&  AppliedFor="germsAndTeeth"  &&  AppliedFor="germsAndTeeth" && AppliedFor="germsAndTeeth" && Vegetarian=true && OrganicType="ayurvedic" &&  OrganicType="ayurvedic" 
+		&& Ingredient="ayurvedic" && Flovor="cinnamon" && price=284 && quantity=480 && available=true &&  ratings=4.5d && Shape="cylindrical" && helpProtect=true &&  Suitable="adults" && Pack=4  &&  Variants="Tulsi,Neem,Clov" &&	color="white")
+		
+		
+	
+		{
+			System.out.println("ToothPaste sigutte");
+		}
+		else
+		{
+			System.out.println("ToothPaste sigalla");
+		}
+	}
+	
+}
+		
